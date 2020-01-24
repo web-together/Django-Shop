46872f50a5810dda9a83775f62721e1060d6bff7
Minchul Kang <tegongkang@gmail.com>
2020-01-24 13:45:32 +0900

    coupon urls

--- djangoshop/urls.py
+++ djangoshop/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),     # social accounts > migrate    
     path('cart/', include('cart.urls')),
+    path('coupon/', include('coupon.urls')),
     path('', include('shop.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
